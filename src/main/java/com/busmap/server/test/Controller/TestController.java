ba0a411c5ccc9cfe9444662522118da92be1b33a
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2024-04-12 22:50:30 +0900

    프론트 연결

--- src/main/java/com/busmap/server/test/Controller/TestController.java
+++ src/main/java/com/busmap/server/test/Controller/TestController.java
@@ -3,12 +3,14 @@ package com.busmap.server.test.Controller;
 import com.busmap.server.test.Service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+//@CrossOrigin(origins = "http://client")
 public class TestController {
 
     private final TestService testService;
